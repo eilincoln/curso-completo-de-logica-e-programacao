4adf0e5fc9c7e997937beac8d750256e12c53f1f
lincoln berto <lincoln.santospro@gmail.com>
2023-11-03 13:31:14 -0300

    adicionando primeiros códigos em linguagem C e colocando um PDF com material do curso. Junto com um HTML documentando as primeiras estruturas de linguagem C

---
+++ 03-linguagem-de-programacao-C/codigos-em-C/estrutura-basica/main.c
@@ -0,0 +1,18 @@
+// Diretivas de pré-processamento
+#include <stdio.h>
+#include <stdlib.h>
+
+//Declarações globais / protótipos de funções
+void imprime();
+
+// Função Principal
+int main()
+{
+    imprime();
+    return 0;
+}
+
+//Outras funções
+void imprime(){
+    printf("Hello World\n");
+}
